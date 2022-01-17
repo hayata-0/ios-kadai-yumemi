de8fcae53b893679a3488c37be6357bcffe1a9dc
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-01-17 21:53:47 +0900

    APIのModelを作成

---
+++ iOSEngineerCodeCheck/Model/SearchRepositoryData.swift
@@ -0,0 +1,46 @@
+//
+//  SearchRepositoryData.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 大野颯太 on 2022/01/17.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+struct SearchRepositories: Decodable {
+    let items: [Repository]
+
+    private enum CodingKeys: String, CodingKey {
+        case items
+    }
+}
+
+struct Repository: Decodable {
+    let id: Int
+    let fullName: String
+    let language: String
+    let stargazersCount: Int
+    let watchersCount: Int
+    let forksCount: Int
+    let openIssuesCount: Int
+
+    let owner: Owner
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case fullName = "full_name"
+        case language = "language"
+        case stargazersCount = "stargazers_count"
+        case watchersCount = "watchers_count"
+        case forksCount = "forks_count"
+        case openIssuesCount = "open_issues_count"
+        case owner = "owner"
+    }
+}
+
+struct Owner: Decodable {
+    let avatarUrl: String
+
+    private enum CodingKeys: String, CodingKey {
+        case avatarUrl = "avatar_url"
+    }
+}
